2cff685cafe0fc31a38ea617486617802a236a63
Ankit Verma <72328368+Ankit732@users.noreply.github.com>
2024-01-05 14:07:26 +0530

    Update SearchBar.jsx

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,52 +1,49 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useContext } from 'react'
-import { myContext } from '../../Context/Context'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { myContext } from '../../Context/Context';
+
 function SearchBar() {
-  const [value,setValue] =useState('')
-  const {product,setProduct}= useContext(myContext)
+  const [value, setValue] = useState('');
+  const { product, setProduct } = useContext(myContext);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get(`https://dummyjson.com/products/search?q=${value}`);
 
-  useEffect(()=>{
-  const fetchData= async ()=>{
-    const data= await axios.get(`https://dummyjson.com/products/search?q=${value}`)
-   
-      setProduct(data.data.products)
-   
-  }
-  fetchData()
-   
-  },[value])
+      setProduct(data.data.products);
+    };
 
-console.log(product)
+    fetchData();
+  }, [value]);
 
-  
-  return     <div className="relative">
-  <input
-    type="text"
-    placeholder="Search..."
-    className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-black w-full"
-    value={value}
-    onChange={(e) => setValue(e.target.value)}
-  />
-  <span className="absolute inset-y-0 right-0 flex items-center pr-3">
-    <svg
-      className="h-5 w-5 text-gray-500 cursor-pointer"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      viewBox="0 0 24 24"
-    >
-      <path d="M22 22l-6-6"></path>
-      <circle cx="10.5" cy="10.5" r="7.5"></circle>
-    </svg>
-  </span>
-</div>
-  
+  console.log(product);
 
-  
+  return (
+    <div className="relative w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-black w-full"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <span className="absolute inset-y-0 right-0 flex items-center pr-3">
+        <svg
+          className="h-5 w-5 text-gray-500 cursor-pointer"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+        >
+          <path d="M22 22l-6-6"></path>
+          <circle cx="10.5" cy="10.5" r="7.5"></circle>
+        </svg>
+      </span>
+    </div>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
