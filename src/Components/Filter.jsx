14335e2b028c864f625c0f7a09fa94c4b31a30bf
Ankit Verma <72328368+Ankit732@users.noreply.github.com>
2024-01-05 14:10:28 +0530

    Update Filter.jsx

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,27 +1,23 @@
-import React, { useState } from 'react'
-import { useContext } from 'react'
-import { myContext } from '../../Context/Context'
+import React, { useContext } from 'react';
+import { myContext } from '../../Context/Context';
+
 function Filter() {
-    const {product,setProduct}= useContext(myContext)
-    const asscending=[...product].sort((a, b) => a.price - b.price)
-    const discending= [...product].sort((a, b) => b.price - a.price)
-  
-    const handleChange=(e)=>{
-      console.log(e)
-      if(e.target.value==="asc"){
-      setProduct(asscending)
-        
-      }
-      else if(e.target.value==="desc"){
-      
-        setProduct(discending)
-      }
-      
+  const { product, setProduct } = useContext(myContext);
+  const ascending = [...product].sort((a, b) => a.price - b.price);
+  const descending = [...product].sort((a, b) => b.price - a.price);
+
+  const handleChange = (e) => {
+    console.log(e);
+    if (e.target.value === 'asc') {
+      setProduct(ascending);
+    } else if (e.target.value === 'desc') {
+      setProduct(descending);
     }
-   
+  };
+
   return (
     <select
-      className="appearance-none bg-slate-50 border border-gray-300 text-black py-2 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:border-blue-500"
+      className="appearance-none bg-slate-50 border border-gray-300 text-black py-2 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:border-blue-500 md:w-1/2 lg:w-1/3 xl:w-1/4"
       onChange={handleChange}
     >
       <option className="text-black" value="" disabled selected>
@@ -34,7 +30,7 @@ function Filter() {
         Price Low to High
       </option>
     </select>
-  )
+  );
 }
 
-export default Filter
+export default Filter;
