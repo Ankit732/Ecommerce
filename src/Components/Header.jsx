85c6b4da7113ac417742662fc0e01c945699aab5
Ankit Verma <72328368+Ankit732@users.noreply.github.com>
2024-01-05 14:13:18 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { useContext } from 'react'
-import { myContext } from '../../Context/Context'
-import { Link } from 'react-router-dom'
-import SearchBar from './SearchBar'
-import Filter from './Filter'
-import Token from './Token'
+import React from 'react';
+import { useContext } from 'react';
+import { myContext } from '../../Context/Context';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
+import Filter from './Filter';
+import Token from './Token';
+
 function Header() {
-  const {cartData}= useContext(myContext)
+  const { cartData } = useContext(myContext);
 
   return (
-    <div className='flex items-center justify-between p-4 bg-gray-800 text-white'>
-      <Link to='/' className='text-xl font-bold'>
-        Home
-      </Link>
+    <div className='flex flex-col md:flex-row items-center justify-between p-4 bg-gray-800 text-white'>
+      <div className='flex items-center'>
+        <Link to='/' className='text-xl font-bold'>
+          Home
+        </Link>
+      </div>
 
-      <div className='flex items-center gap-4'>
+      <div className='flex items-center mt-4 md:mt-0 gap-4'>
         <Link to='/Cart' className='relative'>
           <button className='bg-blue-500 text-white px-4 py-2 rounded-full'>
             Cart ({cartData.length})
@@ -22,12 +25,11 @@ function Header() {
         </Link>
 
         <SearchBar />
-        <Filter/>
-      <Token/>
+        <Filter />
+        <Token />
       </div>
     </div>
-    
-  )
+  );
 }
 
-export default Header
+export default Header;
